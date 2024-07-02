22442309679f3464c803fe1e45d134a05871bb64
default <geenloop@icloud.com>
2024-07-02 09:58:03 +0000

    docker stuff

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+        port: 3454,
+        sockPath: "sockjs-node",
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/'
+      : '/proxy/3000/'
+  }
